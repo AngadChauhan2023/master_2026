9213aea157f5cd5b55c301bc2eec8d9a4bcdccac
Angad Chauhan <122216462+AngadChauhan2023@users.noreply.github.com>
2026-08-18 23:30:03 +0530

    Delete com.lead.management-1 directory

--- com.lead.management-1/src/main/java/com/lead/test/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.lead.test.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lead.test.entity.User;
-
-public interface UserRepo extends JpaRepository<User, Long>{
-
-}
