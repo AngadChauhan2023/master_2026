f89a199446f3dc4aa3cc365c97db4589d0a4ff2d
Angad Chauhan <122216462+AngadChauhan2023@users.noreply.github.com>
2026-08-18 23:30:15 +0530

    Delete com.lead.management directory

--- com.lead.management/src/main/java/com/lead/management/repository/ComplaintRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lead.management.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.lead.management.entity.Complaint;
-@Repository
-public interface ComplaintRepository extends JpaRepository<Complaint, Long> {}
--- com.lead.management/src/main/java/com/lead/management/repository/EnquiryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lead.management.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.lead.management.entity.Enquiry;
-@Repository
-public interface EnquiryRepository extends JpaRepository<Enquiry, Long> {}
